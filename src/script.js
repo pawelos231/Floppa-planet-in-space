42066f0c498b15c5307dbc74eb6047628edf6648
pawelos231 <pawellinek3D@gmail.com>
2022-03-24 15:27:36 +0100

    first commit, floppa in space :D

--- src/script.js
+++ src/script.js
@@ -2,6 +2,15 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
+import { EffectComposer } from "/node_modules/three/examples/jsm/postprocessing/EffectComposer.js";
+import { RenderPass } from "/node_modules/three/examples/jsm/postprocessing/RenderPass.js";
+import { UnrealBloomPass } from "/node_modules/three/examples/jsm/postprocessing/UnrealBloomPass.js";
+
+
+//loading
+const textureLoader = new THREE.TextureLoader()
+const normalTexture = textureLoader.load("/texture/moonmap.png")
+const moon = textureLoader.load("/texture/cover1.jpg")
 
 // Debug
 const gui = new dat.GUI()
@@ -13,25 +22,64 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
-
+const geometry = new THREE.SphereBufferGeometry(.8, 64, 64)
+const sunEl = new THREE.SphereBufferGeometry(1, 64, 64)
 // Materials
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+const material = new THREE.MeshStandardMaterial()
+material.metalness = 1
+material.roughness = 0.8
+material.normalMap = normalTexture
+material.map = moon
+material.color = new THREE.Color(0xffffff)
+
+const materialForSun = new THREE.MeshPhongMaterial()
+materialForSun.roughness = 1
+materialForSun.color = new THREE.Color(0xFDB813)
+materialForSun.emissive = new THREE.Color(0xFDB813)
+materialForSun.emissiveIntensity = 5
+
+
+//render bloom pass
+
+
 
 // Mesh
 const sphere = new THREE.Mesh(geometry,material)
+const sun = new THREE.Mesh(sunEl, materialForSun)
+sun.position.z = -50
+sun.position.x = -40
+sun.position.y = 1
+
 scene.add(sphere)
+scene.add(sun)
 
 // Lights
 
 const pointLight = new THREE.PointLight(0xffffff, 0.1)
 pointLight.position.x = 2
 pointLight.position.y = 3
-pointLight.position.z = 4
+pointLight.position.z = 10
+pointLight.intensity = 0.08
 scene.add(pointLight)
 
+const pointLight2 = new THREE.PointLight(0xfff5c3, 0.1)
+// pointLight.position.x = 2
+// pointLight.position.y = 3
+// pointLight.position.z = 4
+pointLight2.position.set(-40,1,-50)
+pointLight2.intensity = 0.85
+scene.add(pointLight2)
+
+gui.add(pointLight2.position, 'y').min(-300).max(3).step(0.01)
+gui.add(pointLight2.position, 'x').min(-600).max(3).step(0.01)
+gui.add(pointLight2.position, 'z').min(-300).max(3).step(0.01)
+gui.add(pointLight2, 'intensity').min(0).max(10).step(0.01)
+const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+scene.add( light );
+
+const pointLightHelper = new THREE.PointLightHelper(pointLight2, 1)
+scene.add(pointLightHelper)
 /**
  * Sizes
  */
@@ -73,25 +121,63 @@ scene.add(camera)
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    alpha: true
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
+const renderScene = new RenderPass(scene, camera);
+const bloomPass = new UnrealBloomPass(
+  new THREE.Vector2(window.innerWidth, window.innerHeight),
+  1.5,
+  0.4,
+  0.85
+);
+bloomPass.threshold = 0.6;
+bloomPass.strength = 3; //intensity of glow
+bloomPass.radius = 0.5;
+const bloomComposer = new EffectComposer(renderer);
+bloomComposer.setSize(window.innerWidth, window.innerHeight);
+bloomComposer.renderToScreen = true;
+bloomComposer.addPass(renderScene);
+bloomComposer.addPass(bloomPass);
+
 /**
  * Animate
  */
+ let mouseX = 0 
+ let mouseY = 0
+ let targetX = 0
+ let targetY = 0
+
+ const windowHalfX = window.innerWidth/2
+ const windowHalfY = window.innerHeight /2
+
+ const onMouseMovement = (e) =>{
+     mouseX = (e.clientX - windowHalfX)
+     mouseY = (e.clientY - windowHalfY)
+ }
+
+document.addEventListener("mousemove", onMouseMovement)
+
+
+
 
 const clock = new THREE.Clock()
 
 const tick = () =>
 {
-
+    targetX = mouseX * .001
+    targetY = mouseY * .001
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    sphere.rotation.y = .5 * elapsedTime
+    sphere.rotation.y = 2 * elapsedTime
 
+     sphere.rotation.y += .5 * (targetX - sphere.rotation.y)
+     sphere.rotation.x += .10 * (targetY - sphere.rotation.x / 2)
+    //  sphere.position.z += 0.0005 * (targetX - sphere.rotation.x / 2)
     // Update Orbital Controls
     // controls.update()
 
@@ -100,6 +186,7 @@ const tick = () =>
 
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
+    bloomComposer.render();
 }
 
 tick()
